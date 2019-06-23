8392d37d3a3492ba2050a70e11d1f1028d2f7a8e
Mike Young <michael.young@rallyhealth.com>
2019-06-23 09:09:39 -0500

    Hacky fix for scroll position saving

--- src/js/client/main/router.js
+++ src/js/client/main/router.js
@@ -5,31 +5,46 @@ var detailViewOrchestrator = require('./detailViewOrchestrator');
 
 var lastNationalId;
 var landedOnMainView;
+var scrollPos;
 
-router.on('/pokemon/:nationalId', params => {
-  var nationalId = parseInt(params.nationalId, 10);
-  worker.postMessage({
-    type: 'detail',
-    nationalId: nationalId
-  });
-  detailViewOrchestrator.animateInPartOne(nationalId);
-  lastNationalId = nationalId;
-}).on('/', () => {
-  landedOnMainView = true;
-  if (typeof lastNationalId === 'number') { // not initial load
-    detailViewOrchestrator.animateOut(lastNationalId);
-  }
-}).resolve();
+router
+  .on('/pokemon/:nationalId', params => {
+    var nationalId = parseInt(params.nationalId, 10);
+    worker.postMessage({
+      type: 'detail',
+      nationalId: nationalId
+    });
+    detailViewOrchestrator.animateInPartOne(nationalId);
+    lastNationalId = nationalId;
+  })
+  .on('/', () => {
+    landedOnMainView = true;
+    if (typeof lastNationalId === 'number') {
+      // not initial load
+      detailViewOrchestrator.animateOut(lastNationalId);
+    }
+  })
+  .resolve();
 
 function toMonsterDetail(nationalId) {
+  scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+  console.log('here 1', scrollPos);
   router.navigate('/pokemon/' + nationalId);
 }
 
 function toMainView() {
   if (landedOnMainView) {
     history.back();
-  } else { // didn't land on main view, don't do a back action
+  } else {
+    // didn't land on main view, don't do a back action
     router.navigate('/');
+  }
+
+  if (scrollPos) {
+    setTimeout(() => {
+      console.log('here 3');
+      document.documentElement.scrollTop = scrollPos;
+    }, 500);
   }
 }
 
